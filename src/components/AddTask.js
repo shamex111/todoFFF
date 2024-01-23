782710157ffa617de421b84a4405093f01a5d3d5
shamex111 <shamexsx@gmail.com>
2024-01-23 22:42:30 +0300

    sdfds

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -14,10 +14,10 @@ const AddTask = ({setTrueOrFalse, tasks}) => {
             {tasks.length === 0 ?
             <>
             <p className={styles.textImg}>Все задачи в <span className={styles.textImgSpan}>одном месте</span></p>
-            <img className={styles.imgNothing} src='/f59d74ab-51eb-4b71-8bfe-a54286915a45.webp'></img>
+            <img className={styles.imgNothing} src='../assets/bottom.webp'></img>
             </> : ''}
             </>
     )
 }
 
-export default AddTask
+export default AddTask 
